af6920c863a881201eee1f943a983435eb9c4ca6
Bret Gibson <bret.gibson@gmail.com>
2020-02-17 19:26:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,7 +11,7 @@ def find_max_value(array)
   array.length.times do |index|
     numToCompare = array[index]
     if numToCompare > array[index]
-      return numToCompare
+      greatestNum = numToCompare
     end
   end
 end
@@ -19,3 +19,13 @@ end
 def find_min_value(array)
   # Add your solution here
 end
+
+
+# [1,2,3,4]
+# numToCompare = 1
+# array[index] = 1
+# numToCompare > array[index]
+# ==== FALSE
+#
+#
+
